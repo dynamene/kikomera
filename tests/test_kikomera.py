db206ab856f0efc102098688836ec37084f4273a
v1b3m <vibenjamin6@gmail.com>
2019-07-19 14:05:25 +0300

    Update tests

--- tests/test_kikomera.py
+++ tests/test_kikomera.py
@@ -7,18 +7,15 @@ class TestPolygon(unittest.TestCase):
 
     def test_Class(self):
         poly = self.polygon
-        print(poly)
 
     def test_edges(self):
-        new_class = Polygon([Point(1, 2), Point(5, 6), Point(7, 1)])
-        assert type(new_class.edges) is list
+        assert type(self.polygon.edges) is list
 
     def test_contains(self):
-        new_class = Polygon([Point(1, 2), Point(5, 6), Point(7, 1)])
         p1 = Point(3, 4)
         p2 = Point(5, 4)
         p3 = Point(1, 2)
 
-        assert new_class.contains(p1) is True
-        assert new_class.contains(p2) is False
-        assert new_class.contains(p3) is True
+        assert self.polygon.contains(p1) is True
+        assert self.polygon.contains(p2) is False
+        assert self.polygon.contains(p3) is True
